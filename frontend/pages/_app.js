7664bf016823b92a31ad255c36133115662932f0
wildanzr <graita.sukma@gmail.com>
2022-08-22 21:28:45 +0700

    feat: login and register basic form

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import '../styles/globals.css'
+import 'antd/dist/antd.css'
 
 function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    screens: {
+      xs: '480px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1366px',
+      '2xl': '1536px',
+      '3xl': '1920px'
+    }
   },
   plugins: []
 }
