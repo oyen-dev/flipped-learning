bc72baf9a90677123a2a2adfff5faba1ab922c9f
wildanzr <graita.sukma@gmail.com>
2022-08-27 20:07:49 +0700

    feat: active class components

--- frontend/pages/admin/management/guru.js
+++ frontend/pages/admin/management/guru.js
@@ -1,6 +1,7 @@
 import Layout from '../../../components/layout'
 import RecentAccess from '../../../components/table/RecentAccess'
 import Statistic from '../../../components/dashboard/Statistic'
+import ActiveClass from '../../../components/table/ActiveClass'
 
 const guru = () => {
   return (
@@ -13,15 +14,22 @@ const guru = () => {
       ogDescription="Admin page"
       ogImage="https://www.example.com/image.jpg"
     >
-      <div className='w-full grid auto-rows-auto lg:grid-cols-2 gap-5'>
-        <div className='flex flex-col w-full items-start justify-start bg-gray-900 px-5 py-5 rounded-lg'>
-          <p className='text-base font-medium pb-5'>Riwayat Akses Terkini</p>
+      <div className="w-full grid auto-rows-auto lg:grid-cols-2 gap-5">
+        <div className="flex flex-col w-full items-start justify-start bg-gray-900 px-5 py-5 rounded-lg">
+          <p className="text-base font-medium pb-5">Riwayat Akses Terkini</p>
           <RecentAccess />
         </div>
 
-        <div className='flex flex-col w-full items-start justify-start bg-gray-900 px-5 py-5 rounded-lg'>
-          <p className='text-base font-medium pb-5'>Statistik Warga Sekolah</p>
+        <div className="flex flex-col w-full items-start justify-start bg-gray-900 px-5 py-5 rounded-lg">
+          <p className="text-base font-medium pb-5">Statistik Warga Sekolah</p>
           <Statistic />
+        </div>
+      </div>
+
+      <div className="flex flex-col w-full items-start justify-start bg-gray-900 px-5 py-5 rounded-lg">
+        <div className="flex flex-col w-full items-start justify-start overflow-x-auto">
+          <p className="text-base font-medium pb-5">Daftar Kelas Aktif</p>
+          <ActiveClass />
         </div>
       </div>
     </Layout>
