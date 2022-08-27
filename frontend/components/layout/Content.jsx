43c9257941dce6ecee4fe01bb4f6ec27e2f87a7a
wildanzr <graita.sukma@gmail.com>
2022-08-27 20:07:49 +0700

    feat: active class components

--- frontend/components/layout/Content.jsx
+++ frontend/components/layout/Content.jsx
@@ -2,7 +2,7 @@
 const Content = ({ children }) => {
   return (
     <div className="h-screen overflow-y-auto justify-start items-start ml-14 pt-2 px-2 md:ml-64 bg-gray-800">
-      <div className="flex flex-col justify-center px-2 py-2">
+      <div className="flex flex-col items-start justify-center px-2 py-2 space-y-5">
         {children}
       </div>
     </div>
--- frontend/pages/admin/management/siswa.js
+++ frontend/pages/admin/management/siswa.js
@@ -1,6 +1,10 @@
+import ActiveClass from '../../../components/table/ActiveClass'
+
 const siswa = () => {
   return (
-    <div>siswa</div>
+    <div className='w-full px-5 py-5 h-screen bg-gray-400'>
+      <ActiveClass />
+    </div>
   )
 }
 
