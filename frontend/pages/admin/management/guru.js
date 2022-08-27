ecf5ecb741c79ff0b85b8dcd0f1ed8060c933099
wildanzr <graita.sukma@gmail.com>
2022-08-27 09:40:27 +0700

    feat: statistic components

--- frontend/pages/admin/management/guru.js
+++ frontend/pages/admin/management/guru.js
@@ -1,5 +1,6 @@
 import Layout from '../../../components/layout'
 import RecentAccess from '../../../components/table/RecentAccess'
+import Statistic from '../../../components/dashboard/Statistic'
 
 const guru = () => {
   return (
@@ -14,13 +15,13 @@ const guru = () => {
     >
       <div className='w-full grid auto-rows-auto lg:grid-cols-2 gap-5'>
         <div className='flex flex-col w-full items-start justify-start bg-gray-900 px-5 py-5 rounded-lg'>
-          <p className='text-base font-medium'>Riwayat Akses Terkini</p>
+          <p className='text-base font-medium pb-5'>Riwayat Akses Terkini</p>
           <RecentAccess />
         </div>
 
         <div className='flex flex-col w-full items-start justify-start bg-gray-900 px-5 py-5 rounded-lg'>
-          <p className='text-base font-medium'>Statistik Warga Sekolah</p>
-          <RecentAccess />
+          <p className='text-base font-medium pb-5'>Statistik Warga Sekolah</p>
+          <Statistic />
         </div>
       </div>
     </Layout>
