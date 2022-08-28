2b87355bcab962f8974652ab5e1f0fb2f9e5a02b
wildanzr <graita.sukma@gmail.com>
2022-08-28 14:53:18 +0700

    feat: dashboard view

--- frontend/pages/admin/management/guru.js
+++ frontend/pages/admin/management/guru.js
@@ -15,21 +15,25 @@ const guru = () => {
       ogImage="https://www.example.com/image.jpg"
     >
       <div className="w-full grid auto-rows-auto lg:grid-cols-2 gap-5">
-        <div className="flex flex-col w-full items-start justify-start bg-gray-900 px-5 py-5 rounded-lg">
-          <p className="text-base font-medium pb-5">Riwayat Akses Terkini</p>
+        <div className="flex flex-col w-full items-center justify-center bg-gray-900 px-5 py-5 rounded-lg">
+          <p className="text-base font-medium pb-5 mb-0">
+            Riwayat Akses Terkini
+          </p>
           <RecentAccess />
         </div>
 
-        <div className="flex flex-col w-full items-start justify-start bg-gray-900 px-5 py-5 rounded-lg">
-          <p className="text-base font-medium pb-5">Statistik Warga Sekolah</p>
+        <div className="flex flex-col w-full items-center lg:items-start justify-start bg-gray-900 px-5 py-5 rounded-lg">
+          <p className="text-base font-medium pb-5 mb-0">
+            Statistik Warga Sekolah
+          </p>
           <Statistic />
         </div>
       </div>
-
-      <div className="flex flex-col w-full items-start justify-start bg-gray-900 px-5 py-5 rounded-lg">
+      <div className="flex flex-col w-full items-center lg:items-start justify-start bg-gray-900 px-5 py-5 rounded-lg">
+        <p className="text-base font-medium pb-5 mb-0">Daftar Kelas Aktif</p>
         <div className="flex flex-col w-full items-start justify-start overflow-x-auto">
-          <p className="text-base font-medium pb-5">Daftar Kelas Aktif</p>
           <ActiveClass />
+          <div className="flex w-full mt-5 lg:mt-0 justify-center items-center" />
         </div>
       </div>
     </Layout>
