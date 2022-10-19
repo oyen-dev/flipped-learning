391ce1f057c251fdd4481e44d0342006d8db9590
ahm4ddm <dania9015@gmail.com>
2022-10-19 23:38:04 +0700

    feat: setting up const and context

---
+++ frontend/src/constants/teacherData.js
@@ -0,0 +1,81 @@
+const teachers = [
+  {
+    key: '1',
+    no: '1',
+    name: 'Rahayu Purwanti',
+    email: 'xnitzsche@hotmail.com',
+    telp: '081335488399'
+  },
+  {
+    key: '2',
+    no: '2',
+    name: 'Ida Puspita',
+    email: 'vwaters@becker.info',
+    telp: '081335488389'
+  },
+  {
+    key: '3',
+    no: '3',
+    name: 'Dagel Winarno',
+    email: 'candice79@hotmail.com',
+    telp: '081335488329'
+  },
+  {
+    key: '4',
+    no: '4',
+    name: 'Bagiya Hardiansyah',
+    email: 'schimmel.timmy@parker.com',
+    telp: '081335488311'
+  },
+  {
+    key: '5',
+    no: '5',
+    name: 'Among Salahudin',
+    email: 'eyundt@erdman.com',
+    telp: '081335488326'
+  },
+  {
+    key: '6',
+    no: '6',
+    name: 'Amalia Handayani',
+    email: 'hayes.merritt@treutel.com',
+    telp: '081335488320'
+  },
+  {
+    key: '7',
+    no: '7',
+    name: 'Soleh Siregar',
+    email: 'deon38@yahoo.com',
+    telp: '081335488369'
+  },
+  {
+    key: '8',
+    no: '8',
+    name: 'Wirda Agustina',
+    email: 'bschmeler@roberts.com',
+    telp: '081335488334'
+  },
+  {
+    key: '9',
+    no: '9',
+    name: 'Cindy Pertiwi',
+    email: 'eshanahan@yahoo.com',
+    telp: '081335488460'
+  },
+  {
+    key: '10',
+    no: '10',
+    name: 'Cindy Pertiwi',
+    email: 'zabella14@hagenes.com',
+    telp: '081455488360'
+  },
+  {
+    key: '11',
+    no: '11',
+    name: 'Tirtayasa Saputra',
+    email: 'slarson@kuvalis.com',
+    telp: '081335438360'
+  }
+]
+
+export default teachers
