2f24d27f740c3b7faa6aaf2f578b1e858234b3e2
wildanzr <graita.sukma@gmail.com>
2022-11-17 11:09:32 +0700

    feat: linked attachment to new page

--- frontend/src/components/card/Post.jsx
+++ frontend/src/components/card/Post.jsx
@@ -2,10 +2,11 @@ import { useGlobal } from '../../contexts/Global'
 import { useAuth } from '../../contexts/Auth'
 
 import api from '../../api'
+import { Attachment } from '../../components/others'
 
 import Cookies from 'js-cookie'
 import { Link, useLocation } from 'react-router-dom'
-import { BsCameraVideo, BsFileImage, BsFilePdf, BsFileWord, BsFilePpt, BsFileEarmark, BsPencilSquare, BsTrash, BsFileText } from 'react-icons/bs'
+import { BsPencilSquare, BsTrash, BsFileText } from 'react-icons/bs'
 
 const Post = (props) => {
   const { description, isTask, attachments, _id: postId, setFetchPosts } = props
@@ -90,6 +91,7 @@ const Post = (props) => {
         {attachments.map((attachment) => <Attachment {...attachment} key={attachment._id} />)}
       </div>
 
+      {/* Button for Teacher */}
       {user.role === 'TEACHER' && (
         <div className="flex flex-row space-x-4 w-full items-center justify-end">
           <Link to={`posts/${postId}/edit`} className='flex flex-row space-x-2 items-center justify-center py-1 px-4 font-normal md:py-2 md:px-4 md:font-medium text-white bg-blue-600 hover:text-white hover:bg-blue-800 rounded-lg duration-300 ease-in-out'>
@@ -106,47 +108,15 @@ const Post = (props) => {
         </div>
       )}
 
-      {user.role === 'STUDENT' && isTask
-        ? (
+      {/* Button for Student */}
+      {user.role === 'STUDENT' && isTask && (
         <div className="flex flex-row space-x-4 w-full items-center justify-end">
           <Link to={`tasks/${postId}/submissions`} className='flex flex-row space-x-2 items-center justify-center py-1 px-4 font-normal md:py-2 md:px-4 md:font-medium text-white bg-blue-600 hover:text-white hover:bg-blue-800 rounded-lg duration-300 ease-in-out'>
             <BsFileText className="w-5 h-5 fill-white" />
             <span>Kumpulkan Tugas</span>
           </Link>
         </div>
-          )
-        : null}
-    </div>
-  )
-}
-
-const Attachment = (props) => {
-  const { type, name, url } = props
-
-  const listOfAcceptFiles = [
-    'image/jpeg',
-    'image/png',
-    'image/svg+xml',
-    'video/mp4',
-    'video/quicktime',
-    'video/webm',
-    'application/pdf',
-    'application/msword',
-    'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-    'application/vnd.ms-powerpoint',
-    'application/vnd.openxmlformats-officedocument.presentationml.presentation'
-  ]
-  const indexofType = listOfAcceptFiles.indexOf(type)
-
-  return (
-    <div className="flex flex-row space-x-2 items-center">
-      {indexofType <= 2 && <BsFileImage className="w-5 h-5 fill-black" />}
-      {indexofType > 2 && indexofType <= 5 && <BsCameraVideo className="w-5 h-5 fill-black" />}
-      {indexofType > 5 && indexofType <= 6 && <BsFilePdf className="w-5 h-5 fill-black" />}
-      {indexofType > 6 && indexofType <= 8 && <BsFileWord className="w-5 h-5 fill-black" />}
-      {indexofType > 8 && indexofType <= 10 && <BsFilePpt className="w-5 h-5 fill-black" />}
-      {indexofType > 10 && <BsFileEarmark className="w-5 h-5 fill-black" />}
-      <a href={url} target='_blank' rel='noreferrer' className="mb-0 text-black" download={name}>{name}</a>
+      )}
     </div>
   )
 }
---
+++ frontend/src/components/others/Attachment.jsx
@@ -0,0 +1,36 @@
+import { Link } from 'react-router-dom'
+import { BsCameraVideo, BsFileImage, BsFilePdf, BsFileWord, BsFilePpt, BsFileEarmark } from 'react-icons/bs'
+
+const Attachment = (props) => {
+  const { _id, type, name } = props
+  console.log(props)
+
+  const listOfAcceptFiles = [
+    'image/jpeg',
+    'image/png',
+    'image/svg+xml',
+    'video/mp4',
+    'video/quicktime',
+    'video/webm',
+    'application/pdf',
+    'application/msword',
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    'application/vnd.ms-powerpoint',
+    'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+  ]
+  const indexofType = listOfAcceptFiles.indexOf(type)
+
+  return (
+    <div className="flex flex-row space-x-2 items-center">
+      {indexofType <= 2 && <BsFileImage className="w-5 h-5 fill-black" />}
+      {indexofType > 2 && indexofType <= 5 && <BsCameraVideo className="w-5 h-5 fill-black" />}
+      {indexofType > 5 && indexofType <= 6 && <BsFilePdf className="w-5 h-5 fill-black" />}
+      {indexofType > 6 && indexofType <= 8 && <BsFileWord className="w-5 h-5 fill-black" />}
+      {indexofType > 8 && indexofType <= 10 && <BsFilePpt className="w-5 h-5 fill-black" />}
+      {indexofType > 10 && <BsFileEarmark className="w-5 h-5 fill-black" />}
+      <Link to={`attachment/${_id}`} className="mb-0 text-black">{name}</Link>
+    </div>
+  )
+}
+
+export default Attachment
